b7271dffe58b401d1dfc5912ea88b5e2a72927d9
vagrant <vagrant@homestead>
2021-07-22 06:11:54 +0000

    ログイン機能

--- ManagementApp/resources/views/auth/register.blade.php
+++ ManagementApp/resources/views/auth/register.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 <div class="container">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<script src="/js/app.js" defer></script>
+<link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
